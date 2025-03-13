bcd1b5b01fcac39d409b6f38ee3b15cfe3e33a4d
JuFML <julianafernandezlima@hotmail.com>
2025-03-13 14:34:40 +0100

    feat: add initial routing setup

---
+++ src/router.tsx
@@ -0,0 +1,32 @@
+import { createBrowserRouter } from "react-router"
+import Layout from "./components/layout"
+import Home from "./pages/home"
+import Login from "./pages/login"
+import Admin from "./pages/admin"
+import Network from "./pages/network"
+
+const router = createBrowserRouter([
+  {
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />
+      },
+      {
+        path: "/login",
+        element: <Login />
+      },
+      {
+        path: "/admin",
+        element: <Admin />
+      },
+      {
+        path: "/admin/network",
+        element: <Network />
+      }
+    ]
+  }
+])
+
+export { router }
