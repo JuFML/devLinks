43171d55ce6044a7e9439ed60bdded5c2b529b7e
JuFML <julianafernandezlima@hotmail.com>
2025-06-02 05:18:52 +0200

    chore: install react-toastify for toast notifications

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
 import { RouterProvider } from "react-router"
 import { router } from "./routes/router"
 
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify';
+
 function App() {
 
   return (
     <>
       <RouterProvider router={router} />
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        pauseOnHover
+        draggable
+      />
     </>
   )
 }
